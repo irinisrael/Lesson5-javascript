c2519b2b22efe368b27334ddf06b1f43804d8b36
Irin Israel <irinisrael@icloud.com>
2015-10-08 17:58:10 -0400

    finished all 3 javascript functions

--- functions3_javascript.js
+++ functions3_javascript.js
@@ -1,11 +1,5 @@
-function homeworkGrade(num) {
-	if (num === 10) {
-		console.log("This homework is brilliant and worth a 10!");
-	} else if (num === 9) {
-		console.log("So close, but one little change... I can live with that.");	
-	} else {
-		console.log("C'mon Scotty - that blackjack thing was cool. And it took me a while to figure out. 10?");
-	}
+function look() {
+	alert("You talkin' to me?!");
 }
 
-homeworkGrade(10);
+look();
